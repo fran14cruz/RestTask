56fd75fd32d1cae3c6ae98d9fb57e58f5c435128
Francisco Cruz <fran14.ich@gmail.com>
2019-05-02 03:28:52 +0300

    config

--- src/main/java/me/restapi/controller/TaskController.java
+++ src/main/java/me/restapi/controller/TaskController.java
@@ -3,11 +3,19 @@ package me.restapi.controller;
 import me.restapi.model.Task;
 import me.restapi.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import javax.validation.constraints.NotNull;
+import java.util.Set;
 import java.util.UUID;
 
 @RestController
+@Validated
 public class TaskController {
 
     @Autowired
@@ -19,7 +27,25 @@ public class TaskController {
     }
 
     @GetMapping(value = "/task/{id}")
-    public Task getTask(@PathVariable UUID id) {
-        return taskService.getTask(id);
+    public ResponseEntity<Task> getTask(@PathVariable("id") @NotNull UUID id) {
+
+        Task task = taskService.getTask(id);
+
+        if (task == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(task, HttpStatus.OK);
+    }
+
+    @ExceptionHandler(value = { ConstraintViolationException.class })
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public String handleResourceNotFoundException(ConstraintViolationException e) {
+        Set<ConstraintViolation<?>> violations = e.getConstraintViolations();
+        StringBuilder strBuilder = new StringBuilder();
+        for (ConstraintViolation<?> violation : violations ) {
+            strBuilder.append(violation.getMessage() + "\n");
+        }
+        return strBuilder.toString();
     }
 }
